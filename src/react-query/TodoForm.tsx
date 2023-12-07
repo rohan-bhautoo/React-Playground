edff5d85533d89e35cf2c042e66fc2825183d97a
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-07 21:16:40 +0400

    Reusable API Client

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -30,9 +30,7 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button disabled={addTodo.isPending} className="btn btn-primary">
-            {addTodo.isPending ? "Adding..." : "Add"}
-          </button>
+          <button className="btn btn-primary">Add</button>
         </div>
       </form>
     </>
