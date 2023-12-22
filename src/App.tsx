8879d3437e800cda9da86b1c9aea3b7358ac279b
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-22 17:01:48 +0400

    Custom Provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,18 @@ import tasksReducer from "./state-management/reducers/tasksReducer";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import TasksContext from "./state-management/contexts/taskContext";
-import AuthContext from "./state-management/contexts/authContext";
-import authReducer from "./state-management/reducers/authReducer";
+import AuthProvider from "./state-management/AuthProvider";
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
