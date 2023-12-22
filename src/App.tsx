0cc79103a3eee7c3cbcc5fc94c14186f528d1be1
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-22 18:07:05 +0400

    Organizing Code for Scalability and Maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import AuthProvider from "./state-management/AuthProvider";
-import TasksProvider from "./state-management/TasksProvider";
+import { TasksProvider } from "./state-management/tasks";
 
 function App() {
   return (
--- src/state-management/tasks/TasksProvider.tsx
+++ src/state-management/tasks/TasksProvider.tsx
@@ -1,3 +1,6 @@
+import { ReactNode, useReducer } from "react";
+import TasksContext from "./taskContext";
+
 export interface Task {
   id: number;
   title: string;
@@ -24,4 +27,17 @@ const tasksReducer = (state: Task[], action: TaskAction): Task[] => {
   }
 };
 
-export default tasksReducer;
+interface Props {
+  children: ReactNode;
+}
+
+const TasksProvider = ({ children }: Props) => {
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  return (
+    <TasksContext.Provider value={{ tasks, dispatch }}>
+      {children}
+    </TasksContext.Provider>
+  );
+};
+
+export default TasksProvider;
