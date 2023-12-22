13805013b0baaa0048358ba8b1be8453f41e4958
Rohan Bhautoo <rohanbhautoo@gmail.com>
2023-12-22 15:46:19 +0400

    Working with Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { useReducer } from "react";
 import "./App.css";
-import LoginStatus from "./state-management/LoginStatus";
 import tasksReducer from "./state-management/reducers/tasksReducer";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import TasksContext from "./state-management/contexts/taskContext";
+import AuthContext from "./state-management/contexts/authContext";
+import authReducer from "./state-management/reducers/authReducer";
 
 function App() {
-  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
+  const [user, authDispatch] = useReducer(authReducer, "");
 
   return (
-    <TasksContext.Provider value={{ tasks, dispatch }}>
-      <NavBar />
-      <HomePage />
-    </TasksContext.Provider>
+    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+      <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
--- src/state-management/TaskList.tsx
+++ src/state-management/TaskList.tsx
@@ -1,12 +1,15 @@
 import { useContext, useReducer } from "react";
 import tasksReducer from "./reducers/tasksReducer";
 import TasksContext from "./contexts/taskContext";
+import AuthContext from "./contexts/authContext";
 
 const TaskList = () => {
   const { tasks, dispatch } = useContext(TasksContext);
+  const { user } = useContext(AuthContext);
 
   return (
     <>
+      <p>User: {user}</p>
       <button
         onClick={() =>
           dispatch({
